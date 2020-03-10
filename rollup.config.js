8f99dda3de0efd961681bee4e71da90df954fda8
Priit Puru <purupriit@gmail.com>
2020-03-10 11:48:29 +0200

    Squashed 'lib/t-components/' changes from 6c76431..e4ab077
    e4ab077 trying out stuff
    
    git-subtree-dir: lib/t-components
    git-subtree-split: e4ab077d58ca54e0b2970a1d1e59496797924876

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import typescript from '@rollup/plugin-typescript';
 export default {
   // If using any exports from a symlinked project, uncomment the following:
   // preserveSymlinks: true,
-	input: ['src/index.js'],
+	input: ['./index.js'],
 	output: {
 		file: 'build/index.js',
 		format: 'es',
@@ -12,7 +12,7 @@ export default {
 	},
 	plugins: [
     babel(),
-    typescript(),
+    // typescript(),
     resolve()
   ]
 };
