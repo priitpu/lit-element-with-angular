09fd2245f08d7440f66acb895cc224bb7f8c6173
Priit Puru <purupriit@gmail.com>
2020-03-10 14:38:59 +0200

    Squashed 'lib/t-components/' changes from 8980dac..c0c28ab
    c0c28ab hmmmmmmm
    
    git-subtree-dir: lib/t-components
    git-subtree-split: c0c28abb0ca763259a90aa2e95ff21e2b7d2b534

--- src/elements/t-input/t-input.ts
+++ src/elements/t-input/t-input.ts
@@ -12,29 +12,37 @@ export class TButton extends LitElement {
     `;
   }
 
-  set value(value: string) {
+  public set value(value: string) {
+    const oldValue = this.inputValue;
     this.inputValue = value;
-    const event = new Event('change');
+    this.requestUpdate('inputValue', oldValue);
+    const event = new Event('input');
     this.dispatchEvent(event);
   }
 
-  get value() {
+  public get value() {
     return this.inputValue;
   }
 
   valueChanged(e: any) {
+    console.log('koerajalad');
     this.inputValue = e.target.value;
-    const event = new Event('change');
+    const event = new Event('input');
     this.dispatchEvent(event);
   }
+
+  updated() {
+    console.log('updated t-input');
+    return;
+  }
+
   render() {
     return html`
       <label>
         <span><slot></slot></span>
         <input
           .value=${this.inputValue}
-          @keyup=${(e: any) => { this.valueChanged(e) }}
-          @change=${(e: any) => { this.valueChanged(e) }}
+          @input=${(e: any) => { this.valueChanged(e) }}
           type="text"
         />
       </label>
